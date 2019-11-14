57ed0909086d373c24959e24a3ba78235274da68
Anton Mitsengendler <a.mic@rtc.ru>
2019-11-14 17:57:30 +0300

    Worker class containing worker-thread state

---
+++ GZippy/Dispatcher.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GZippy
+{
+    class Dispatcher
+    {
+        public Dispatcher()
+        {
+            _workers = new Worker[Environment.ProcessorCount];
+            for (int i = 0; i < _workers.Length; i++)
+            {                
+                _workers[i] = new Worker();
+            }
+        }
+
+
+        private readonly Worker[] _workers;
+        private readonly ConcurrentQueue<Worker> _activeJobs = new ConcurrentQueue<Worker>();
+        private readonly AutoResetEvent _chunkReady = new AutoResetEvent(false);
+
+        private const long ChunkLength = 512;        
+        private long sourcePosition;              
+
+
+        public void Compress(Stream source, Stream destination)
+        {   
+            for(int i = 0; i < _workers.Length; i++)
+            {
+                _workers[i].QueueJob(
+                    (worker) =>  EnqueueWorkItem(worker,source),
+                    (data) => Compress(data),
+                    OnChunkCompleted
+                    );                    
+            }
+        }
+
+        private void OnChunkCompleted()
+        {
+            
+        }
+
+        private bool IsNextChunkReady()
+        {
+            Worker worker;
+            if(_activeJobs.TryPeek(out worker))
+            {
+                return worker.HasResult;
+            }
+            return false;
+        }
+
+        private byte[] Compress(byte[] data)
+        {
+            return data;
+        }
+
+        private byte[] EnqueueWorkItem(Worker worker, Stream source)
+        {             
+            lock (_activeJobs)
+            {
+                if (sourcePosition >= source.Length)
+                    return null;
+                var currentChunkLength = sourcePosition + ChunkLength > source.Length ?
+                    source.Length - source.Position :
+                    ChunkLength;
+                var payload = new byte[currentChunkLength];
+                source.Read(payload,0,payload.Length);
+                _activeJobs.Enqueue(worker);
+                return payload;
+            }
+        }
+    }
+}
---
+++ GZippy/Worker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GZippy
+{
+    class Worker : IDisposable
+    {
+        public Worker()
+        {
+            _workerThread = new Thread(WorkerRoutine) { IsBackground = true };
+            _readyToWork = new ManualResetEvent(false);
+            _workerThread.Start();            
+        }
+
+        private void WorkerRoutine()
+        {
+            while(true)
+            {                
+                _readyToWork.WaitOne();                
+                // Try to aquire new payload, if no payload then go to sleep
+                var payload = _payloadSource(this);
+                if(payload == null)
+                {
+                    State = WorkerState.Idle;
+                    _readyToWork.Reset();
+                    _payloadSource = null;
+                    _job = null;
+                    _jobCompleted = null;
+                    continue;
+                }
+                var result = _job(payload);
+                _results.Enqueue(result);
+                _jobCompleted?.Invoke();                
+            }
+        }
+      
+        private readonly Thread _workerThread;
+        private readonly ManualResetEvent _readyToWork;        
+        private readonly ConcurrentQueue<byte[]> _results;        
+
+        
+        private Func<Worker,byte[]> _payloadSource;
+        private Func<byte[],byte[]> _job;
+        private Action _jobCompleted;
+        
+
+        
+        public WorkerState State { get; private set; }
+
+        /// <summary>
+        /// Worker will query bytes from payloadSource and pass to job untill payloadSource returns null
+        /// </summary>
+        /// <param name="payloadSource">source of payload to work with</param>
+        /// <param name="job">data processing routine</param>
+        public void QueueJob(Func<Worker, byte[]> payloadSource, Func<byte[],byte[]> job, Action onComplete)
+        {
+            if(State != WorkerState.Idle)
+                throw new ConcurrencyException("You have started new job without finishing the previos one.");
+            _payloadSource = payloadSource;
+            _job = job;
+            _jobCompleted = onComplete;
+            _readyToWork.Set();
+        }        
+
+        public bool HasResult => _results.Count > 0;
+        public byte[] GetResult()
+        {
+            byte[] result;
+            if(_results.TryDequeue(out result))
+            {
+                return result;
+            }
+            throw new ConcurrencyException("Result was requested before it is ready");
+        }
+
+        public void Dispose()
+        {
+            
+        }       
+    }
+
+    enum WorkerState
+    {
+        Idle,
+        Busy
+    }
+}
